388832c02d0a9bf4b68b8f830ce1312c5b0115cf
Keshav <keshavmehra2005@gmail.com>
2024-11-25 21:54:15 +0530

    Added Saturation,Hue,Blur and Grayscale

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,34 @@ function App() {
       value: "100",
       range: {min: 0 , max: 200},
       unit: '%'
+    },
+    {
+      name: "Saturation",
+      property: "saturate",
+      value: "100",
+      range: {min: 0 , max: 200},
+      unit: '%'
+    },
+    {
+      name: "Hue",
+      property: "hue-rotate",
+      value: "0",
+      range: {min: 0 , max: 360},
+      unit: "deg"
+    },
+    {
+      name: "Blur",
+      property: "blur",
+      value: "0",
+      range: {min: 0 , max: 20},
+      unit: "px"
+    },
+    {
+      name: "Grayscale",
+      property: "grayscale",
+      value: "0",
+      range: {min: 0 , max: 100},
+      unit: "%"
     }
   ]
 
